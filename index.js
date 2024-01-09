be4dc6de1d655f018745d4bff8d315b7d796b7b6
himanshurelhan70 <himanshurelh70@gmail.com>
2024-01-09 10:56:21 +0530

    creating webhook for ZOHO MA

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+
+app.post('/getData', (req, res) => {
+    console.log("hello");
+    // console.log(req);
+
+    res.send("received data");
+})
+
+app.get("/", (req,res) => {
+    console.log("welcome to the app");
+
+    res.send("Yes its working");
+})
+
+
+app.listen(3000, () => {
+    console.log("app is running");
+})
